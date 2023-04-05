3afdfe996f48f8be8fa481fd42723222632f05ee
kolczov94 <kolczov94@yandex.ru>
2023-04-05 20:08:44 +0300

    feat: Добавить API

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
 import AppHeader from "../app-header/app-header";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import styles from "./app.module.css";
-import { CUSTOM_DATA } from "../../utils/custom-data";
+import { URL_API } from "../../utils/constants";
+
+const modalRoot = document.getElementById("react-modals");
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(URL_API)
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setIngredients(data);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+
   return (
     <div className={styles.app}>
       <AppHeader />
       <main className={`${styles.main} pl-5 pr-5 pb-10`}>
-        <BurgerIngredients ingredients={CUSTOM_DATA} />
-        <BurgerConstructor ingredients={CUSTOM_DATA} />
+        <BurgerIngredients ingredients={ingredients} />
+        <BurgerConstructor ingredients={ingredients} />
       </main>
     </div>
   );
